997e99d0062f33d9a75ac5bc83f94260531ac0b1
Yulian-21 <uliandius@gmail.com>
2022-07-25 22:35:35 +0300

    Create a UserType

---
+++ src/components/Users/UserType/UserType.ts
@@ -0,0 +1,10 @@
+export default interface IUser{
+    id: number
+    userName: string
+    firstName: string
+    lastName: string
+    email: string
+    password: string
+    phone: string
+    userStatus: number
+}
