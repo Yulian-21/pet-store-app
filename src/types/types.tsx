f01ea778a9c53fe5fa38551891cd86bf345ac9a0
Yulian-21 <uliandius@gmail.com>
2022-07-25 18:02:05 +0300

    update types

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -16,3 +16,12 @@ export interface IPet{
     tags: Array<ITag>
     status: string
 }
+
+export interface IOrder{
+    id: number
+    petId: number
+    quantity: number
+    shipDate: string
+    status: string
+    complete: boolean
+}
