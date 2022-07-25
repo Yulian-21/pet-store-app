eeb3129dc8d676e0eae5fad935e44b409d998137
Yulian-21 <uliandius@gmail.com>
2022-07-25 18:03:19 +0300

    addd order item

--- src/components/Orders/OrderItem/OrderItem.tsx
+++ src/components/Orders/OrderItem/OrderItem.tsx
@@ -1,8 +1,43 @@
-import React, {FC} from 'react'
+import axios from 'axios'
+import React, {FC, useEffect, useState} from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { IOrder, IPet } from '../../../types/types'
+
+type PetItemPageParams = {
+    id: string
+  }
 
 const OrderItem: FC = () => {
-  return (
-    <div>OrderItem</div>
+    const [order, setOrder] = useState<IOrder>()
+    const { id } = useParams<PetItemPageParams>()
+    const history = useNavigate()
+  
+    useEffect(() => {
+      fetchOrder();
+    }, [])
+  
+  
+    const fetchOrder = async () => {
+      try{
+      const orderResponse = await axios.get<IOrder>('https://petstore3.swagger.io/api/v3/store/order/' + id)
+      setOrder(orderResponse.data);
+      } catch(err) {
+        alert(err)
+      }
+    }
+
+   
+  
+    return (
+      <div>
+        <h1> The data page of order №{order?.id} </h1>
+         <div>
+            <>Pet Id: {order?.petId}, quantity: {order?.quantity}</>
+            <>Order Status: {order?.status} Ship Date: {order?.shipDate}</>
+            Completed: <input type="checkbox" checked={order?.complete} />
+         </div>
+          <button onClick={() => history('/orders')}> Return</button>
+      </div>
   )
 }
 
