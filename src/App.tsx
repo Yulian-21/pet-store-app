cc31327884a87e78e701ccf3e295cb15bb6d2bc5
Yulian-21 <uliandius@gmail.com>
2022-07-25 18:07:35 +0300

    update routes in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import PetsPage from './components/Pets/PetsPage';
 import {Route, Routes, useLocation} from 'react-router-dom';
 import PetItemPage from './components/Pets/PetItemPage';
 import Navigation from './components/Navigation/Navigation';
+import StorePage from './components/Orders/StorePage';
+import OrderItem from './components/Orders/OrderItem/OrderItem';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
       <Route element={<PetsPage />} path={'/pets'} />
       <Route element={<PetItemPage />} path={'/pets/:id'} />
+      <Route element={<StorePage />} path={'/orders'} />
+      <Route element={<OrderItem />} path={'/orders/:id'} />
       </Routes>
       </div>
     </div>
