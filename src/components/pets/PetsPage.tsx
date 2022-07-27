3259281d1e61aa42b6c9428470276f6b626ce032
Yulian-21 <uliandius@gmail.com>
2022-07-27 20:48:02 +0300

    Rewrite label text + Add select with statuses

--- src/components/pets/PetsPage.tsx
+++ src/components/pets/PetsPage.tsx
@@ -1,23 +1,34 @@
-import React, {FC, useState} from 'react'
+import React, { FC, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { Status } from './PetTypes/PetTypes'
 
 const PetsPage: FC = () => {
-    const [petId, setPetId] = useState('')
-    const [petTag, setPetTag] = useState('')  
+  const [petId, setPetId] = useState('')
+  const [petTag, setPetTag] = useState('')
+  const [status, setStatus] = useState('')
 
-    const navigation = useNavigate()
+  const navigation = useNavigate()
   return (
     <div>
-        <div>
-          <label htmlFor="petId">Id of pet that interests you</label>
-        <input type="text" value={petId} id="petId" onChange={(changing) => setPetId(changing.target.value)} placeholder="Pet Id"/>
+      <div>
+        <label htmlFor="petId">Search By Id</label>
+        <input type="text" value={petId} id="petId" onChange={(changing) => setPetId(changing.target.value)} placeholder="Pet Id" />
         <button onClick={() => navigation('/pets/' + petId)}>Show</button>
-        </div>
-        <div>
-        <label htmlFor="petTag">Interests you Tag</label>
-        <input type="text" value={petTag} id="petTag" onChange={(changing) => setPetTag(changing.target.value)} placeholder="Tag"/>
-        <button onClick={() => navigation('/pets/tag/' + petTag)}>Show</button>
-        </div>
+      </div>
+      <div>
+        <label htmlFor="petTag">Search by Tag</label>
+        <input type="text" value={petTag} id="petTag" onChange={(changing) => setPetTag(changing.target.value)} placeholder="Tag" />
+        <button onClick={() => navigation('/pets/byTag/' + petTag)}>Show</button>
+      </div>
+      <div>
+        <label htmlFor="status">Search By Status</label>
+        <select id="status" onChange={(changing) => setStatus(changing.target.value)}>
+          <option value={Status.Available}>Available</option>
+          <option value={Status.Pending}>Pending</option>
+          <option value={Status.Sold}>Sold</option>
+        </select>
+        <button onClick={() => navigation('/pets/byTag/' + petTag)}>Search</button>
+      </div>
     </div>
   )
 }
