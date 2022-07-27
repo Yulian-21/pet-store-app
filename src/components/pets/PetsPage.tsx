1d50b5245cfd63dbaad0b470b9beb3d2af2ecf3e
Yulian-21 <uliandius@gmail.com>
2022-07-27 17:04:32 +0300

    Add fields to Tag Search

--- src/components/pets/PetsPage.tsx
+++ src/components/pets/PetsPage.tsx
@@ -1,13 +1,23 @@
 import React, {FC, useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const PetsPage = () => {
+const PetsPage: FC = () => {
     const [petId, setPetId] = useState('')
+    const [petTag, setPetTag] = useState('')  
+
     const navigation = useNavigate()
   return (
     <div>
-        <input type="text" value={petId} onChange={(changing) => setPetId(changing.target.value)}/>
+        <div>
+          <label htmlFor="petId">Id of pet that interests you</label>
+        <input type="text" value={petId} id="petId" onChange={(changing) => setPetId(changing.target.value)} placeholder="Pet Id"/>
         <button onClick={() => navigation('/pets/' + petId)}>Show</button>
+        </div>
+        <div>
+        <label htmlFor="petTag">Interests you Tag</label>
+        <input type="text" value={petId} id="petTag" onChange={(changing) => setPetId(changing.target.value)} placeholder="Tag"/>
+        <button onClick={() => navigation('/pets/' + petId)}>Show</button>
+        </div>
     </div>
   )
 }
