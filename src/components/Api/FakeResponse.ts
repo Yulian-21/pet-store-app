a1bfbee8fefe418aa9db48ff37b7f35ad4b2ba28
Yulian-21 <uliandius@gmail.com>
2022-07-29 00:34:27 +0300

    Remove and revorked Fake response

--- src/components/Api/FakeResponse.ts
+++ src/components/Api/FakeResponse.ts
@@ -2,15 +2,15 @@ import { AxiosResponse } from "axios";
 import { resolve } from "path";
 import IUser from '../Users/UserType/UserType';
 
-export const userResponseHandler = (data: IUser) => {
+export const responseHandler = <T>(data: T): AxiosResponse => {
   
     return { status: 200, statusText: "OK", data: data, headers: {}, config: {}, request:{}}
 }
 
-export const promisify = (data: IUser): Promise<AxiosResponse> => {
+export const promisify = <T>(data: T): Promise<AxiosResponse> => {
          return new Promise((resolve, reject) => {
             setTimeout(()=> {
-                resolve(userResponseHandler(data));
+                resolve(responseHandler(data));
             }, 1000)
          })
         }
