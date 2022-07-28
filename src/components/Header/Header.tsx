c8ec778e69edaa6e5525c3491384b21f46377dea
Yulian-21 <uliandius@gmail.com>
2022-07-29 00:36:54 +0300

    Replace navbar from navigation place into header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import h from './Header.module.css'
 
 const Header = () => {
   return (
     <header className={h.header}>
-     <img src='https://static.vecteezy.com/system/resources/previews/006/470/722/original/pet-shop-logo-design-template-modern-animal-icon-label-for-store-veterinary-clinic-hospital-shelter-business-services-flat-illustration-background-with-dog-cat-and-horse-free-vector.jpg'/>
+      <img src='https://static.vecteezy.com/system/resources/previews/006/470/722/original/pet-shop-logo-design-template-modern-animal-icon-label-for-store-veterinary-clinic-hospital-shelter-business-services-flat-illustration-background-with-dog-cat-and-horse-free-vector.jpg' />
+      <ul className={h.menu}>
+        <li className={h.fontStyle}>
+          <NavLink className={navData => navData.isActive ? h.active : h.fontStyle} to='/pets'>Pets</NavLink>
+        </li>
+        <li className={h.fontStyle}>
+          <NavLink className={navData => navData.isActive ? h.active : h.fontStyle} to='/orders'>Orders</NavLink>
+        </li>
+        <li className={h.fontStyle}>
+          <NavLink className={navData => navData.isActive ? h.active : h.fontStyle} to='/users'>Users</NavLink>
+        </li>
+        <li className={h.fontStyle}>
+          <NavLink className={navData => navData.isActive ? h.active : h.fontStyle} to='/about'>About</NavLink>
+        </li>
+      </ul>
     </header>
   )
 }
