ec1c04e40cd6b0b865ef6ede2a15052f235146f1
Yulian-21 <uliandius@gmail.com>
2022-07-28 16:46:51 +0300

    Add reusable form

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import {
+    useForm,
+    UseFormReturn,
+    SubmitHandler,
+    UseFormProps,
+    FormProvider,
+    FieldValues
+} from "react-hook-form";
+
+type FormProps = {
+    onSubmit: any;
+    children:
+    | React.ReactNode
+    | ((methods: UseFormReturn<FieldValues>) => React.ReactNode);
+    options?: UseFormProps<FieldValues>;
+    id?: string;
+};
+
+export const Form = ({
+    onSubmit,
+    children,
+    options,
+    id,
+}: FormProps) => {
+    const methods = useForm({
+        ...options
+    });
+    return (
+        <FormProvider {...methods}>
+            <form onSubmit={methods.handleSubmit(onSubmit)} id={id}>
+                {typeof children === "function" ? children(methods) : children}
+            </form>
+        </FormProvider>
+    );
+};
