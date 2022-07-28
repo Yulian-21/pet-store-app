11fe37856877c14be0165dc6d368f8110e35568d
Yulian-21 <uliandius@gmail.com>
2022-07-29 00:37:59 +0300

    create new component add image

---
+++ src/components/pets/AddImage/AddImage.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@mui/material';
+import Reac, {useState} from 'react'
+import CustomInput from '../../Form/CustomInput'
+import { Form } from '../../Form/Form';
+
+const AddImage = () => {
+  
+  return (
+                <div>
+                   
+                </div>
+  )
+}
+
+export default AddImage
