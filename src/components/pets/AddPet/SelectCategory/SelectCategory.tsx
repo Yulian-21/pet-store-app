4b99ce582682651e620ed8df92ac91ff88bedd53
Yulian-21 <uliandius@gmail.com>
2022-07-31 22:49:23 +0300

    Add SelectCategory component and implement logic

---
+++ src/components/pets/AddPet/SelectCategory/SelectCategory.tsx
@@ -0,0 +1,45 @@
+import { InputLabel, MenuItem, Select } from '@mui/material'
+import React from 'react'
+import { Control, Controller, FieldValues } from 'react-hook-form'
+
+type sCProps = {
+    control: Control<FieldValues, any>
+    name: string
+}
+
+const SelectCategory = (props: sCProps) => {
+    const {control, name} = props
+    return (
+        <>
+        <Controller
+            control={control}
+            name={name}
+            render={({
+                field: { onBlur, value, onChange, name, ref },
+                fieldState: { invalid, isTouched, isDirty, error },
+                formState,
+            }) => (
+                
+                <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={value}
+                label="Category"
+                onChange={onChange}
+                sx={{width: "200px", backgroundColor: "rgba(255,255,255,0.5)",marginTop: "10px"}}
+            >
+                <MenuItem value={"Cat"} onSelect={(value) => onChange(value)}>Cats</MenuItem>
+                <MenuItem value={"Dog"} onSelect={(value) => onChange(value)}>Dogs</MenuItem>
+                <MenuItem value={"Bird"} onSelect={(value) => onChange(value)}>Birds</MenuItem>
+                <MenuItem value={"string"} onSelect={(value) => onChange(value)}>Exotic</MenuItem>
+            </Select>
+                
+            )}
+        />
+
+            
+        </>
+    )
+}
+
+export default SelectCategory
