8604bc0aba8fd02354d7f03735686bc7a789e96f
Yulian-21 <uliandius@gmail.com>
2022-08-03 15:21:17 +0300

    Add new usable types

--- src/components/pets/PetTypes/PetTypes.ts
+++ src/components/pets/PetTypes/PetTypes.ts
@@ -1,37 +1,58 @@
-export interface ICategory{
-    id?: number;
-    name: string;
+import { Control, FieldValues } from "react-hook-form";
+
+export interface ICategory {
+  id?: number;
+  name: string;
 }
 
-export interface ITag{
-    id?: number,
-    name: string,
+export interface ITag {
+  id?: number;
+  name: string;
 }
 
-export interface IPet{
-    id?: number,
-    name: string,
-    category: ICategory
-    photoUrls: Array<string>
-    tags: Array<ITag>,
-    status: string,
+export interface IPet {
+  id?: number;
+  name: string;
+  category: ICategory;
+  photoUrls: Array<string>;
+  tags: Array<ITag>;
+  status: string;
 }
 
-export type PetItemPageParams =
- {
-  id: string
-}
+export type PetItemPageParams = {
+  id: string;
+};
 
 export type PetByTagParams = {
-    tag: string
-}
+  tag: string;
+};
 
 export type PetByStatusParams = {
-    status: string
+  status: string;
+};
+
+export type handleData = {
+  id: number;
+  name: string;
+  photoUrls: string;
+  category: string;
+  status: string;
+  tags: ITag[];
+};
+
+export enum Status {
+  Available = "available",
+  Pending = "pending",
+  Sold = "sold",
 }
 
-export enum Status{
-    Available= "available",
-    Pending="pending",
-    Sold = "sold"
-}
+export type partialData = {
+  id: number;
+  name: string;
+  status: string;
+};
+
+export type selectProps = {
+  control: Control<FieldValues, any>;
+  name: string;
+};
