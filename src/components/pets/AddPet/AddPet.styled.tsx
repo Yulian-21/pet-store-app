fefa2adb8154dc91cef13dd2908bec23c97dfd77
Yulian-21 <uliandius@gmail.com>
2022-08-15 12:01:58 +0300

    Styled AddPet Component

--- src/components/pets/AddPet/AddPet.styled.tsx
+++ src/components/pets/AddPet/AddPet.styled.tsx
@@ -1,12 +1,6 @@
 import styled from 'styled-components';
 import { Form } from "../../Form/Form";
 
-const Title = styled("h1")`
-  font-size: 1.5em;
-  text-align: center;
-  color: palevioletred;
-`;
-
 export const StyledForm = styled(Form)`
   font-size: 1.5em;
   text-align: center;
