8cf13b1f43ee6cb019ca36220b8c20cf193c3a89
Yulian-21 <uliandius@gmail.com>
2022-08-15 12:02:55 +0300

    Implement Delete Pet Request

--- src/components/pets/DeletePet/DeletePet.tsx
+++ src/components/pets/DeletePet/DeletePet.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { Button, Input, InputLabel } from '@mui/material'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import { BASE_API_URL } from '../../../config';
 
 const DeletePet = () => {
+    const [id, setId] = useState('');
+
+    //useEffect(() => { deletePet() }, [id])
+    const deletePet = async () => {
+        await axios.delete(BASE_API_URL + "/pet/" + id)
+            .then((response) => { alert(response.status); })
+            .catch(err => { alert(err.message) });
+    }
+
     return (
-        <div>DeletePet</div>
+        <>
+            <InputLabel htmlFor='id' sx={{ fontWeight: 'bold' }}>Input deleting pet id</InputLabel>
+            <Input name='petId' id="id" placeholder='Id' value={id} onChange={(id) => setId(id.target.value)} />
+            <Button onClick={deletePet}>Delete</Button>
+        </>
     )
 }
 
